f2552b025ab490c47cf0963c939c40e314859d02
Adrian Papari <a@onedaybeard.net>
2024-08-17 08:39:54 +0200

    wip fx-chart example

--- examples/fx-chart.rs
+++ examples/fx-chart.rs
@@ -13,13 +13,13 @@ use ratatui::backend::CrosstermBackend;
 use ratatui::buffer::Buffer;
 use ratatui::{Frame, widgets};
 use ratatui::layout::{Margin, Offset, Position, Rect};
-use ratatui::style::Color;
-use ratatui::widgets::{Clear, StatefulWidget, Widget};
+use ratatui::style::{Color, Style};
+use ratatui::widgets::{Block, Clear, StatefulWidget, Widget};
 
 use Interpolation::*;
 use tachyonfx::{BufferRenderer, CenteredShrink, Effect, EffectRenderer, fx, Interpolation, Shader, EffectTimer};
 use tachyonfx::CellFilter::{AllOf, Inner, Not, Outer, Text};
-use tachyonfx::fx::{effect_fn, never_complete, parallel, sequence, term256_colors, with_duration};
+use tachyonfx::fx::{effect_fn, never_complete, parallel, sequence, term256_colors, with_duration, Direction};
 use tachyonfx::widget::EffectTimeline;
 use crate::gruvbox::Gruvbox::{Dark0Hard, Dark0Soft};
 
@@ -32,15 +32,49 @@ type Terminal = ratatui::Terminal<CrosstermBackend<Stdout>>;
 
 struct App {
     last_tick: Duration,
-    use_aux_buffer: bool, /** demonstrates reusing aux_buffer contents for render */
+    use_aux_buffer: bool,
     aux_buffer: Rc<RefCell<Buffer>>,
     inspected_effect: Effect,
+    screen_area: Rect,
     timeline: EffectTimeline,
 }
 
 #[derive(Default)]
-struct StatefulWidgets {
-    effects: Vec<Effect>
+struct Effects {
+    aux_buf_fx: Option<Effect>,
+    post_process: Vec<Effect>,
+}
+
+impl Effects {
+    fn push(&mut self, effect: Effect) {
+        self.post_process.push(effect);
+    }
+
+    fn process_active_fx(
+        &mut self,
+        duration: Duration,
+        buf: &mut Buffer,
+        area: Rect
+    ) {
+        self.post_process.iter_mut()
+            .for_each(|effect| { effect.process(duration, buf, area); });
+
+        self.post_process.retain(|effect| !effect.done());
+    }
+
+    fn process_buf_fx(
+        &mut self,
+        duration: Duration,
+        buf: &mut Buffer,
+        area: Rect
+    ) {
+        if let Some(effect) = self.aux_buf_fx.as_mut() {
+            effect.process(duration, buf, area);
+            if effect.done() {
+                self.aux_buf_fx = None;
+            }
+        }
+    }
 }
 
 impl App {
@@ -50,9 +84,10 @@ impl App {
         let fx = example_complex_fx();
         Self {
             last_tick: Duration::ZERO,
-            use_aux_buffer: false,
+            use_aux_buffer: true,
             aux_buffer: Rc::new(RefCell::new(Buffer::empty(aux_buffer_area))),
             timeline: EffectTimeline::from(&fx),
+            screen_area: Rect::default(),
             inspected_effect: fx,
         }
     }
@@ -61,8 +96,96 @@ impl App {
         let effect = self.inspected_effect.clone();
 
         let mut buf = self.aux_buffer.borrow_mut();
+        Block::new()
+            .style(Style::default().bg(Color::Black))
+            .render(buf.area, &mut buf);
+
         EffectTimeline::from(&effect)
             .render(buf.area, &mut buf);
+    }
+}
+
+mod effects {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use std::time::Duration;
+    use crossterm::style::Color;
+    use ratatui::buffer::Buffer;
+    use ratatui::layout::{Offset, Rect};
+    use tachyonfx::{fx, CellFilter, Effect, Interpolation};
+    use tachyonfx::fx::*;
+    use tachyonfx::Interpolation::{BounceIn, BounceInOut, BounceOut, CircIn, CircOut, ExpoIn, ExpoInOut, ExpoOut, QuadIn, QuadInOut, QuadOut, QuartIn, QuartOut, SineIn};
+
+    pub(super) fn out_fx_1(area: Rect) -> Effect {
+        let step = Duration::from_millis(100);
+        let bg = Color::Black;
+
+        with_duration(step * 15, parallel(vec![
+            never_complete(dissolve(123, (step * 5, ExpoInOut))),
+            never_complete(fade_to_fg(bg, (5 * step, BounceOut))),
+        ]).with_area(area))
+    }
+
+    pub(super) fn tree_fx_1(area: Rect) -> Effect {
+        let step = Duration::from_millis(100);
+        let bg = Color::Black;
+
+        parallel(vec![
+            coalesce(100, (step * 5, ExpoInOut))
+                .with_cell_selection(CellFilter::Text),
+            sweep_in(Direction::UpToDown, 1, bg, step * 3),
+        ]).with_area(area)
+    }
+
+    pub(super) fn chart_fx_1(area: Rect) -> Effect {
+        let step = Duration::from_millis(100);
+        let bg = Color::Black;
+
+        sequence(vec![
+            timed_never_complete(step * 3, fade_to_fg(bg, 0)),
+            sweep_in(Direction::LeftToRight, 20, bg, step * 5),
+        ]).with_area(area)
+    }
+
+    pub(super) fn chart_fx_2(area: Rect) -> Effect {
+        let step = Duration::from_millis(100);
+        let bg = Color::Black;
+
+        sequence(vec![
+            timed_never_complete(step * 3, fade_to_fg(bg, 0)),
+            fade_from_fg(bg, (step * 7, ExpoOut)),
+        ]).with_area(area)
+    }
+
+    pub(super) fn chart_fx_3(area: Rect) -> Effect {
+        let step = Duration::from_millis(100);
+        let bg = Color::Black;
+
+        let hsl_shift = [0.0, -100.0, -50.0];
+
+        parallel(vec![
+            // initially desaturated
+            sequence(vec![
+                timed_never_complete(9 * step, hsl_shift_fg(hsl_shift, 0)),
+                hsl_shift_fg(hsl_shift, (12 * step, SineIn)).reversed()
+            ]),
+            sequence(vec![
+                timed_never_complete(step * 3, fade_to_fg(bg, 0)),
+                sweep_in(Direction::LeftToRight, 20, bg, step * 5),
+            ]),
+        ]).with_area(area)
+    }
+
+    pub(super) fn move_in_fx(direction: Direction, buf: Rc<RefCell<Buffer>>) -> Effect {
+        let screen = buf.borrow().area;
+        let offset: Offset = match direction {
+            Direction::LeftToRight => Offset { x: -(screen.width as i32), y: 0 },
+            Direction::RightToLeft => Offset { x: screen.width as i32, y: 0 },
+            Direction::UpToDown    => Offset { x: 0, y: -(screen.height as i32) },
+            Direction::DownToUp    => Offset { x: 0, y: screen.height as i32 },
+        };
+
+        translate_buf(offset, buf.clone(), (250, CircIn)).reversed()
     }
 }
 
@@ -70,11 +193,9 @@ fn main() -> Result<()> {
     let mut terminal = setup_terminal()?;
 
     // create app and run it
-    let app = App::new(Rect::new(0, 0, 80, 40));
+    let app = App::new(Rect::new(0, 0, 100, 40));
     let res = run_app(&mut terminal, app);
 
-    // widgets
-    let mut stateful_widgets = StatefulWidgets::default();
 
     // restore terminal
     disable_raw_mode()?;
@@ -92,7 +213,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-pub type OffscreenBuffer = Rc<RefCell<Buffer>>;
+pub type AuxBuffer = Rc<RefCell<Buffer>>;
 
 fn run_app(
     terminal: &mut Terminal,
@@ -100,19 +221,62 @@ fn run_app(
 ) -> io::Result<()> {
     let mut last_frame_instant = std::time::Instant::now();
 
+    let mut effects = Effects::default();
+    // effects.add(effects::tree_fx_1(&app.inspected_effect, terminal.size()));
     app.refresh_aux_buffer();
 
     loop {
         app.last_tick = last_frame_instant.elapsed();
         last_frame_instant = std::time::Instant::now();
-        terminal.draw(|f| ui(f, &mut app))?;
+        terminal.draw(|f| {
+            app.screen_area = f.area();
+            ui(f, &app, &mut effects)
+        })?;
 
         if event::poll(Duration::from_millis(32))? {
             if let Event::Key(key) = event::read()? {
                 if key.kind == KeyEventKind::Press {
+
+                    let active_area = || {
+                        if app.use_aux_buffer {
+                            app.aux_buffer.borrow().area
+                        } else {
+                            app.screen_area
+                        }
+                    };
+
                     match key.code {
                         KeyCode::Esc       => return Ok(()),
                         KeyCode::Char(' ') => app.refresh_aux_buffer(),
+                        KeyCode::Char('1') => {
+                            let inspected = &app.inspected_effect;
+                            let rects = EffectTimeline::from(inspected).layout(active_area());
+                            effects.push(effects::tree_fx_1(rects.tree))
+                        },
+                        KeyCode::Char('2') => {
+                            let inspected = &app.inspected_effect;
+                            let rects = EffectTimeline::from(inspected).layout(active_area());
+                            effects.push(effects::chart_fx_1(rects.chart))
+                        },
+                        KeyCode::Char('3') => {
+                            let inspected = &app.inspected_effect;
+                            let rects = EffectTimeline::from(inspected).layout(active_area());
+                            effects.push(effects::chart_fx_2(rects.chart))
+                        },
+                        KeyCode::Char('4') => {
+                            effects.aux_buf_fx = Some(effects::move_in_fx(Direction::LeftToRight, app.aux_buffer.clone()))
+                        },
+                        KeyCode::Char('5') => {
+                            effects.aux_buf_fx = Some(effects::move_in_fx(Direction::UpToDown, app.aux_buffer.clone()))
+                        },
+                        KeyCode::Char('6') => {
+                            effects.push(effects::out_fx_1(active_area()))
+                        },
+                        KeyCode::Char('7') => {
+                            let inspected = &app.inspected_effect;
+                            let rects = EffectTimeline::from(inspected).layout(active_area());
+                            effects.push(effects::chart_fx_3(rects.chart))
+                        },
                         KeyCode::Tab       => app.use_aux_buffer = !app.use_aux_buffer,
                         _ => {}
                     }
@@ -124,18 +288,31 @@ fn run_app(
 
 fn ui(
     f: &mut Frame,
-    app: &mut App,
+    app: &App,
+    effects: &mut Effects
 ) {
-    if f.size().area() == 0 { return; }
+    let rect = f.area();
+    if rect.area() == 0 { return; }
 
-    Clear.render(f.size(), f.buffer_mut());
+    let buf: &mut Buffer = f.buffer_mut();
+    Clear.render(rect, buf);
 
     if app.use_aux_buffer {
-        app.aux_buffer.render_buffer(Offset::default(), f.buffer_mut());
+        if effects.aux_buf_fx.is_some() {
+            effects.process_buf_fx(app.last_tick, buf, rect);
+        } else {
+            app.aux_buffer.render_buffer(Offset::default(), buf);
+        }
     } else {
+        Block::new()
+            .style(Style::default().bg(Color::from_u32(0x201416)))
+            .render(rect, buf);
+
         app.timeline.clone()
-            .render(f.size(), f.buffer_mut());
+            .render(rect, buf);
     }
+
+    effects.process_active_fx(app.last_tick, buf, rect);
 }
 
 
