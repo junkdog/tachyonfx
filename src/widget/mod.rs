473d7da961230e9c652b1fb0c638ff3773037207
Adrian Papari <a@onedaybeard.net>
2024-08-20 15:43:16 +0200

    fix tests

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,4 +1,4 @@
-mod effect_chart;
+mod effect_timeline;
 mod effect_span;
 mod color_registry;
 mod cell_filter_registry;
@@ -8,7 +8,7 @@ pub(crate) use effect_span::EffectSpan;
 pub(crate) use cell_filter_registry::CellFilterRegistry;
 pub(crate) use color_registry::ColorRegistry;
 
-pub use effect_chart::{
+pub use effect_timeline::{
     EffectTimeline,
     EffectTimelineRects
 };
